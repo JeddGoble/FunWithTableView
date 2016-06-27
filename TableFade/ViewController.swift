9d373892d67ff4c6eb6f1d271ae30e755bed6bab
Jedd Goble <jedd.goble@teamaol.com>
2016-06-27 15:00:08 -0700

    Initial Commit

---
+++ TableFade/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableFade
+//
+//  Created by jgoble52 on 6/27/16.
+//  Copyright © 2016 tangital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
